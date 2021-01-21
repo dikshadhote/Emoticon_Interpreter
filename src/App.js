0df8bf480f990dfe37bda0e8d5deceb5a7e3c2c6
dikshadhote <39937404+dikshadhote@users.noreply.github.com>
2021-01-21 17:25:46 +0530

    op on screen input processing using useState() and giving op on screen

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
-var count = 0;
-function btnClickHandler() {
-  count++;
-  alert(count);
-}
+
 export default function App() {
+  //usestate function returns two values so we store it in array.
+  //The array value must not change so it is declare as const
+  const [likeCounter, setLikeCounter] = useState(0);
+  function btnClickHandler() {
+    var newLikeCounterValue = likeCounter + 1;
+    setLikeCounter(newLikeCounterValue);
+  }
   return (
     <div className="App">
       <h1>Inside out!!</h1>
       <button onClick={btnClickHandler}>Hit like!</button>
+      {likeCounter}
     </div>
   );
 }
